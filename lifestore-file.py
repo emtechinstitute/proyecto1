5c1d194a2a344c1decfb289915b1024ede2e1e76
emtechinstitute <69870228+emtechinstitute@users.noreply.github.com>
2020-08-18 14:08:45 -0500

    Create lifestore-file.py

---
+++ lifestore-file.py
@@ -0,0 +1,14 @@
+  
+"""
+This is the LifeStore-SalesList data:
+lifestore-sales = [id search, content search, date, hour (24:00)]
+lifestore-searches = [id sale, id product, id user, score (from 1 to 10), returned (true or false)]
+lifestore-products = [id product, name, description, category, on stock]
+
+ARCHIVO PENDIENTE DE ACTUALIZACIÓN
+
+"""
+
+lifestore-sales = []
+lifestore-searches = []
+lifestore-products = []
